05b8e3811c7fb5359415ffbe9149321c00e5f930
comet-a1 <ren20080810@icloud.com>
2025-02-01 20:34:52 +0900

    userモデルの単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname { Faker::Name.unique.name }
+    email { Faker::Internet.unique.email }
+    password { 'a1b2c3' }
+    password_confirmation { 'a1b2c3' }
+    birth_date { Faker::Date.birthday }
+    last_name { '山田' }
+    first_name { '太郎' }
+    last_name_kana {'ヤマダ' }
+    first_name_kana { 'タロウ' }
+  end
+end
