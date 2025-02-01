611f4bc3b8c169cba249bb6e2085c99d2bb64d25
comet-a1 <ren20080810@icloud.com>
2025-02-01 21:15:12 +0900

    Revert "rubocopの実行"
    This reverts commit 1f412ac0097bea1ba522319390e5b0f36c8e8b20.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,15 +2,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
 
   # 新規登録後のリダイレクト先
-  def after_sign_up_path_for(_resource)
+  def after_sign_up_path_for(resource)
     root_path
   end
 
   protected
 
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date,
-                                             :email, :password])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date, :email, :password])
   end
 end
