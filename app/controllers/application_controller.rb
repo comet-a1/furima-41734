9377387c6c11f715d6e3c919ab646b9831f92e84
comet-a1 <ren20080810@icloud.com>
2025-02-03 11:12:32 +0900

    修正.3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birth_date, :last_name, :first_name, :last_name_kana, :first_name_kana])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :birth_date, :last_name, :first_name, :last_name_kana, :first_name_kana])
   end
 end
