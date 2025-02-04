2476630c1bfed50b4f0917fd8045361f231599b3
comet-a1 <ren20080810@icloud.com>
2025-02-04 19:40:01 +0900

    validates設定とエラーメッセージの表示

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_03_034738) do
   end
 
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description", null: false
     t.integer "category_id", null: false
     t.integer "price", null: false
@@ -58,8 +58,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_03_034738) do
     t.integer "shipping_fee_id", null: false
     t.integer "shipping_origin_id", null: false
     t.integer "shipping_days_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "shipping_days", charset: "utf8mb3", force: :cascade do |t|
@@ -97,4 +99,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_03_034738) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
