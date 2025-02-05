4842e9e4ee5c99b4830fb6ca97dea1d9ea527ec6
comet-a1 <ren20080810@icloud.com>
2025-02-05 15:54:40 +0900

    修正.1

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe Item, type: :model do
     end
 
     context '出品できない場合' do
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+
       it '画像が添付されていないと出品できない' do
         @item.image = nil
         @item.valid?
