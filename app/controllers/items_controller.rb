6f08329edbff007f2013ec180c9e7a9f3d13ecfb
comet-a1 <ren20080810@icloud.com>
2025-02-07 15:43:19 +0900

    ログイン状態によってのアクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_if_not_seller, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -20,19 +22,16 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
-      render :new, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -41,5 +40,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :price, :image, :category_id, :condition_id, :shipping_fee_id,
                                  :shipping_origin_id, :shipping_days_id).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_if_not_seller
+    if @item.user != current_user
+      redirect_to root_path
+    end
   end
 end
