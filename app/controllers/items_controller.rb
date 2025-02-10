4583a30b4b41e5c8d85a5a3895f365d9879b197a
comet-a1 <ren20080810@icloud.com>
2025-02-10 16:25:39 +0900

    販売状況によっての表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :redirect_if_not_seller, only: [:edit, :update, :destroy]
+  before_action :redirect_if_sold_out, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -55,5 +56,11 @@ class ItemsController < ApplicationController
     return unless @item.user != current_user
 
     redirect_to root_path
+  end
+
+  def redirect_if_sold_out
+    if @item.sold_out?
+      redirect_to root_path
+    end
   end
 end
