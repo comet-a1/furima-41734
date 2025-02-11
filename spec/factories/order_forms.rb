516d5a35ca7ca0c20fca6c10c446258d1123b939
comet-a1 <ren20080810@icloud.com>
2025-02-11 18:36:48 +0900

    order_form_modelテストコード

---
+++ spec/factories/order_forms.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_form do
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+    postal_code { '123-4567' }
+    shipping_origin_id { 1 }
+    city { 'Tokyo' }
+    address_line1 { 'aoyama 1-1-1' }
+    address_line2 { 'Building A' }
+    phone_number { '09012345678' }
+  end
+end
